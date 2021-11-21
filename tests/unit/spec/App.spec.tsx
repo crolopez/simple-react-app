1cb61350487547e9733a24846748830a45c98e48
Cristobal Lopez <crolopezp@gmail.com>
2021-11-21 07:27:40 -0800

    Reposition test file

--- tests/unit/spec/App.spec.tsx
+++ tests/unit/spec/App.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { App } from './App'
+import { App } from '../../../src/App'
 
 test('renders application title', () => {
   render(<App title={ 'Simple React Application on test' }/>)
