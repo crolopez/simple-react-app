f0e7091295162c16ae159b2000a83f25b3587c6c
Cristobal Lopez <crolopezp@gmail.com>
2021-11-23 13:00:08 -0800

    Enable or disable the buttons depending on the form state

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useContext, useRef } from 'react'
+import { ChangeEvent, useContext, useEffect, useRef } from 'react'
 import { TaskStateContext, TaskSubmitHandlerContext } from '../Contexts'
 import { defaultTask } from '../helpers/defaultTask'
 
@@ -6,7 +6,10 @@ type HandleInputChange = ChangeEvent<HTMLInputElement|HTMLTextAreaElement>
 type ButtonClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>
 
 export default function TaskForm(): JSX.Element {
-  const inputTitle = useRef<HTMLInputElement>(null)
+  const headerRef = useRef<HTMLHeadingElement>(null)
+  const saveButtonRef = useRef<HTMLButtonElement>(null)
+  const updateButtonRef = useRef<HTMLButtonElement>(null)
+  const inputTitleRef = useRef<HTMLInputElement>(null)
   const taskSubmitHandler = useContext(TaskSubmitHandlerContext)
   const formTaskState = useContext(TaskStateContext)
 
@@ -20,33 +23,48 @@ export default function TaskForm(): JSX.Element {
     e.preventDefault()
     taskSubmitHandler(formTaskState.task)
     formTaskState.setTask(defaultTask)
-    inputTitle.current?.focus()
+    inputTitleRef.current?.focus()
   }
 
   const handleClearTask = (e: ButtonClickEvent) => {
     e.preventDefault()
     formTaskState.setTask(defaultTask)
-    inputTitle.current?.focus()
+    inputTitleRef.current?.focus()
   }
+
+  useEffect(() => {
+    if (headerRef.current == null
+      || saveButtonRef.current == null
+      || updateButtonRef.current == null) return
+
+    const newTask = formTaskState.task.id == 0
+
+    headerRef.current.textContent = newTask
+      ? 'Create a new task': 'Update the task'
+    saveButtonRef.current.hidden = !newTask
+    updateButtonRef.current.hidden = newTask
+  })
 
   return (
     <div className='card card-body bg-none'>
-      <h1>Add Task</h1>
+      <h1 ref={headerRef}></h1>
 
       <form>
         <input name='title' type="text" placeholder='Write a title'
           className='form-control mb-3 shadow-none border-0'
           onChange={handleInputChange} value={formTaskState.task.title}
-          autoFocus ref={inputTitle}/>
+          autoFocus ref={inputTitleRef}/>
 
         <textarea name="description" rows={2} placeholder='Write a description'
           className='form-control mb-3 shadow-none border-0'
           onChange={handleInputChange} value={formTaskState.task.description}/>
 
-        <button className='btn btn-success' onClick={handleTaskSubmit}>
+        <button className='btn btn-success' onClick={handleTaskSubmit}
+          ref={saveButtonRef}>
           Save
         </button>
-        <button className='btn btn-secondary' onClick={handleTaskSubmit}>
+        <button className='btn btn-secondary' onClick={handleTaskSubmit}
+          ref={updateButtonRef}>
           Update
         </button>
         <button className='btn btn-warning' onClick={handleClearTask}>
